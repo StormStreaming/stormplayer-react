e095a76d76be95f4a80aa3a8b90a34f54500b500
Damian Ceglarz <damianceglarz@192.168.0.161>
2023-07-18 00:04:16 +0200

    chore: fix attempts

--- src/components/stormPlayer/StormPlayer.tsx
+++ src/components/stormPlayer/StormPlayer.tsx
@@ -28,4 +28,6 @@ const StormPlayer = forwardRef<StormPlayerClass, Props>(
   }
 );
 
+console.log("StormPlayer");
+
 export default StormPlayer;
--- src/index.dev.tsx
+++ src/index.dev.tsx
@@ -9,10 +9,10 @@ const libraryConfig = {
     serverList: [
       // list of streaming server, 2nd, 3rd etc. will be used as backup
       {
-        host: "localhost", // host or ip to the streaming server
+        host: "cdn-eu01.stormstreaming.com", // host or ip to the streaming server
         application: "live", // application name (can be configured in storm server settings)
-        port: 80, // server port
-        ssl: false, // whenever SSL connection should be used or not
+        port: 443, // server port
+        ssl: true, // whenever SSL connection should be used or not
       },
     ],
     sourceList: [
@@ -23,19 +23,19 @@ const libraryConfig = {
     ],
   },
   settings: {
-  autoStart: true, // if true, video will start playing automatically, but will be muted too
-  restartOnError: true, // if something bad happens, player will try to restart
-  reconnectTime: 1.0, // if a connection with a server fails, player will restart in given time
-  enabledProtocols: ["MSE", "HLS"], // "MSE" for desktop, android browsers and iPad OS, "HLS" for iPhone iOS
-  video: {
-    scalingMode: "letterbox", // possible values "fill", "letterbox", "crop" and "letterbox"
-  },
-  debug: {
-    console: {
-      // console output
-      enabled: true, // if console output is activated
+    autostart: true, // if true, video will start playing automatically, but will be muted too
+    restartOnError: true, // if something bad happens, player will try to restart
+    reconnectTime: 1.0, // if a connection with a server fails, player will restart in given time
+    enabledProtocols: ["MSE", "HLS"], // "MSE" for desktop, android browsers and iPad OS, "HLS" for iPhone iOS
+    video: {
+      scalingMode: "letterbox", // possible values "fill", "letterbox", "crop" and "letterbox"
     },
-  },
+    debug: {
+      console: {
+        // console output
+        enabled: true, // if console output is activated
+      },
+    },
   },
 };
 
