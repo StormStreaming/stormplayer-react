fbeedc89b1e95802f75565f6e51fe74f5e780fde
Damian Ceglarz <damianceglarz@Damians-MacBook-Pro.local>
2023-07-31 09:20:41 +0200

    chore: make it work...

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import fs from 'fs';
+import fs from "fs";
 import css from "rollup-plugin-import-css";
 import livereload from "rollup-plugin-livereload";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import serve from "rollup-plugin-serve";
-import url from '@rollup/plugin-url';
 
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
-import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import url from "@rollup/plugin-url";
 
-const packageJSONRaw = fs.readFileSync('./package.json', 'utf8');
+const packageJSONRaw = fs.readFileSync("./package.json", "utf8");
 const pkg = JSON.parse(packageJSONRaw);
 
 const NODE_ENV_PRODUCTION = "production";
@@ -25,15 +25,15 @@ const config = {
   input: inputFileName,
   plugins: [
     nodeEnv === "production" && peerDepsExternal(),
+    resolve({
+      browser: true,
+      extensions,
+    }),
     replace({
       "process.env.NODE_ENV": JSON.stringify(nodeEnv),
       preventAssignment: true,
     }),
     json(),
-    resolve({
-      browser: true,
-      extensions,
-    }),
     commonjs({
       include: "**/node_modules/**",
     }),
@@ -44,13 +44,13 @@ const config = {
     }),
     css(),
     nodeEnv !== "production" &&
-    serve({
-      open: true,
-      verbose: true,
-      contentBase: ["", "public"],
-      host: "localhost",
-      port: 3000,
-    }),
+      serve({
+        open: true,
+        verbose: true,
+        contentBase: ["", "public"],
+        host: "localhost",
+        port: 3000,
+      }),
     url({
       include: ["**/*.ttf", "**/*.woff", "**/*.woff2"],
       limit: Infinity,
@@ -58,29 +58,28 @@ const config = {
     nodeEnv !== "production" && livereload(),
   ],
   output:
-      nodeEnv === "production"
-          ? [
-            {
-              format: "es",
-              dir: pkg.module.replace(/\/index.js$/, ""),
-              sourcemap: false,
-            },
-            {
-              file: pkg.main,
-              format: "cjs",
-              sourcemap: false,
-              plugins: [typescript()],
-            },
-          ]
-          : [
-            {
-              format: "iife",
-              name: "stormPlayer",
-              dir: pkg.module.replace(/\/index.js$/, ""),
-              sourcemap: true,
-
-            },
-          ],
+    nodeEnv === "production"
+      ? [
+          {
+            format: "es",
+            dir: pkg.module.replace(/\/index.js$/, ""),
+            sourcemap: false,
+          },
+          {
+            file: pkg.main,
+            format: "cjs",
+            sourcemap: false,
+            plugins: [typescript()],
+          },
+        ]
+      : [
+          {
+            format: "iife",
+            name: "stormPlayer",
+            dir: pkg.module.replace(/\/index.js$/, ""),
+            sourcemap: true,
+          },
+        ],
 };
 
 export default config;
